d3c3228f17b453d97375413a5ae4c335d202ac04
Jonathan Woo <jonghun.digitalfactory@gmail.com>
2019-08-05 01:02:23 +0900

    comment 추가

--- bank.py
+++ bank.py
@@ -7,42 +7,58 @@ Covers:
 - Condition events
 
 Scenario:
-  A counter with a random service time and customers who renege. Based on the
-  program bank08.py from TheBank tutorial of SimPy 2. (KGM)
+  A counter with a random service time and customers who renege
+
+  Based on the program bank08.py from TheBank tutorial of SimPy 2
+
+  https://pythonhosted.org/SimPy/Tutorials/TheBank.html
 
 """
 import random
 
 import simpy
 
-RANDOM_SEED = 42
-NEW_CUSTOMERS = 5  # Total number of customers
-INTERVAL_CUSTOMERS = 10.0  # Generate new customers roughly every x seconds
-MIN_PATIENCE = 1  # Min. customer patience
-MAX_PATIENCE = 3  # Max. customer patience
+RANDOM_SEED = 42 # 난수 지정 번호 - 변경에 따라 결과가 달라짐
+NO_CUSTOMERS = 10  # 총 고객 수
+INTERVAL_CUSTOMERS = 10.0  # 도착간 시간 간격 (inter-arrival time)
+MIN_PATIENCE = 1  # Minimum customer patience
+MAX_PATIENCE = 3  # Maximum customer patience
 
 
 def source(env, number, interval, counter):
-    """Source generates customers randomly"""
+
+    # Random하게 고객(customer) 생성(generation)
     for i in range(number):
-        c = customer(env, 'Customer%02d' % i, counter, time_in_bank=12.0)
-        env.process(c)
+
+        # customer 함수(고객 이름, 리소스, 업무시간)를 프로세스에 탑재
+        # customer 함수(name, resource, working time)를 프로세스에 탑재
+        env.process(customer(env, 'Customer%02d' % i, counter, time_in_bank=12.0))
+
+        #도착간 시간간격 시간 진행
         t = random.expovariate(1.0 / interval)
         yield env.timeout(t)
 
-
 def customer(env, name, counter, time_in_bank):
-    """Customer arrives, is served and leaves."""
+    # source에서 customer 함수 호출과 함께 작동
+    # 도착한 시간을 arrive에 기록
     arrive = env.now
     print('%7.4f %s: Here I am' % (arrive, name))
 
+    # 본 함수를 호출한 source에서 전달받은 counter resource에 대한 requirement 구문
     with counter.request() as req:
+
+        # customer가 기다릴 수 있는 한계 시간 생성
         patience = random.uniform(MIN_PATIENCE, MAX_PATIENCE)
-        # Wait for the counter or abort at the end of our tether
+
+        # counter가 가용해질때까지 기다리거나(req) 또는 인내심의 한계를 넘어 기다림 포기(env.timeout(patience))
         results = yield req | env.timeout(patience)
 
+        # 기다린 시간을 wait에 기록
         wait = env.now - arrive
 
+        # yield의 리턴값으로 받은 results에 req가 있는지 없는지에 따라 동작 결정
+        # req in results가 true이면 time_in_bank 시간만큼 은행 업무 수행
+        # 그렇지 않으면(else) 포기하고 돌아간 것으로 가정하고 메세지만 print
         if req in results:
             # We got to the counter
             print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
@@ -63,5 +79,5 @@ env = simpy.Environment()
 
 # Start processes and run
 counter = simpy.Resource(env, capacity=1)
-env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, counter))
-env.run()
+env.process(source(env, NO_CUSTOMERS, INTERVAL_CUSTOMERS, counter))
+env.run(200)
