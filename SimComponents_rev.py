fae891f05eb9803e578e4e3d9d5917a6d669c0b6
jonathan.woo <jonghun.digitalfactory@gmail.com>
2019-09-23 15:37:24 +0900

    upgrade timeout loop through catch of event finish time

--- SimComponents_rev.py
+++ SimComponents_rev.py
@@ -14,6 +14,9 @@ import math
 import time
 import threading
 
+COUNT = 0
+COUNT2 = 0
+
 class Part(object):
     """ A very simple class that represents a packet.
         This packet will run through a queue at a switch output port.
@@ -95,7 +98,16 @@ class Source(object):
             if (self.out.__class__.__name__ == 'Process'):
                 while len(self.out.store.items) >= self.out.qlimit - 1:
                     print('start loop at', self.__class__.__name__, self.env.now)
-                    yield self.env.timeout(0.1)
+                    # source에서 loop가 실행되는 횟수
+                    global COUNT
+                    COUNT += 1
+                    #timeout 0.1로 사용한 경우 loop 약 50000번, 수정한 경우 loop 횟수 약 1500번
+                    print(f'num source loop: {COUNT}')
+                    #후공정과 동일한 시간에 event를 만들면 후공정보다 먼저 실행되는 경우가 있어 미소값을 더해줌
+                    yield self.env.timeout(self.out.next_run - self.env.now + 1e-6)
+                    #queue에서 가장 빠른 이벤트를 가져오는 경우 모든 이벤트를 체크하므로 loop 횟수가 많아지므로 비효율적임
+                    #yield self.env.timeout(self.env._queue[0][0] - self.env.now)
+                    #yield self.env.timeout(0.1)
                     print('end loop')
 
             self.out.put(p)
@@ -186,6 +198,7 @@ class Process(object):
         self.busy = 0  # Used to track if a part is currently being sent
         self.action = env.process(self.run())  # starts the run() method as a SimPy process
         self.working_time = 0
+        self.next_run = 0.0 #process에서 다음 이벤트가 발생하는 시간을 저장하여 wait until에 활용
         print(self.name)
 
     def run(self):
@@ -197,6 +210,7 @@ class Process(object):
             proc_time = self.rate()
 
             self.start_time = self.env.now
+            self.next_run = self.env.now + proc_time
             yield self.env.timeout(proc_time)
             self.working_time += self.env.now - self.start_time
 
@@ -204,7 +218,16 @@ class Process(object):
                 print(self.name, 'qlimit :', self.qlimit, 'queue length :', len(self.store.items))
                 while len(self.out.store.items) >= self.out.qlimit - 1:
                     print('start loop at', self.name, self.env.now)
-                    yield self.env.timeout(0.1)
+                    #process에서 loop가 실행되는 횟수
+                    global COUNT2
+                    COUNT2 += 1
+                    # timeout 0.1로 사용한 경우 loop 약 50000번, 수정한 경우 loop 횟수 약 1000번
+                    print(f'num process loop: {COUNT2}')
+                    #후공정때문에 대기해야하는 경우 next_run을 후공정의 다음 이벤트까지로 업데이트 해줌
+                    self.next_run += self.out.next_run - self.env.now + 1e-6
+                    # 후공정과 동일한 시간에 event를 만들면 후공정보다 먼저 실행되는 경우가 있어 미소값을 더해줌
+                    yield self.env.timeout(self.out.next_run - self.env.now + 1e-6)
+                    #yield self.env.timeout(0.1)
                     print('end loop')
 
             self.out.put(msg)
