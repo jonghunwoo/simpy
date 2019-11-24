a674eeb02aa08c746cb2060f398405be3d1ba8b1
woojonghun <jonghun.digitalfactory@gmail.com>
2019-11-24 18:17:50 +0900

    revised

--- procurement.py
+++ procurement.py
@@ -6,7 +6,7 @@ import csv
 import pandas as pd
 import functools
 import simpy
-from SimComponents_rev5 import Source, DataframeSource, Sink, Process, Monitor
+from SimComponents_for_supply_chain import Source, DataframeSource, Sink, Process, Monitor
 
 """ Data loading
     1. request raw excel file from github of jonghunwoo
@@ -38,9 +38,9 @@ data = pd.read_csv('./data/spool_data_for_simulation.csv')
 
 df = data[["NO_SPOOL", "DIA", "Length", "Weight", "MemberCount", "JointCount", "Material", "제작협력사", "도장협력사", "Actual_makingLT", "Predicted_makingLT", "Actual_paintingLT", "Predicted_paintingLT"]]
 
-df.rename(columns={'NO_SPOOL': 'part_no', "제작협력사": 'proc1', '도장협력사': 'proc2', 'Actual_makingLT': 'ct1', 'Predicted_makingLT': 'ct2', 'Actual_paintingLT': 'ct3', 'Predicted_paintingLT': 'ct4'}, inplace=True)
+df.rename(columns={'NO_SPOOL': 'part_no', "제작협력사": 'proc1', '도장협력사': 'proc2', 'Actual_makingLT': 'ct3', 'Predicted_makingLT': 'ct1', 'Actual_paintingLT': 'ct4', 'Predicted_paintingLT': 'ct2'}, inplace=True)
 
-print(df["proc1"])
+#print(df["proc1"])
 
 """ Simulation
     Dataframe with product data is passed to Source.
@@ -49,32 +49,49 @@ print(df["proc1"])
 """
 
 random.seed(42)
-adist = functools.partial(random.randrange,3,7) # Inter-arrival time
+adist = functools.partial(random.randrange,1,3) # Inter-arrival time
 samp_dist = functools.partial(random.expovariate, 1) # need to be checked
 proc_time = functools.partial(random.normalvariate,5,1) # sample process working time
 
-RUN_TIME = 10
+RUN_TIME = 45000
+#RUN_TIME = 5000
 
 env = simpy.Environment()
 
 Source = DataframeSource(env, "Source", adist, df, 7)
 Sink = Sink(env, 'Sink', debug=False, rec_arrivals=True)
 
-Proc1_1 = Process(env, "(주)성광테크", proc_time, 10, 7, qlimit=10000, limit_bytes=False)
-Proc1_2 = Process(env, "건일산업(주)", proc_time, 10, 7, qlimit=10000, limit_bytes=False)
-Proc1_3 = Process(env, "부흥", proc_time, 10, 7, qlimit=10000, limit_bytes=False)
-Proc1_4 = Process(env, "삼성중공업(주)거제", proc_time, 10, 7, qlimit=10000, limit_bytes=False)
-Proc1_5 = Process(env, "성일", proc_time, 10, 7, qlimit=10000, limit_bytes=False)
-Proc1_6 = Process(env, "성일SIM함안공장", proc_time, 10, 7, qlimit=10000, limit_bytes=False)
-Proc1_7 = Process(env, "해승케이피", proc_time, 10, 7, qlimit=10000, limit_bytes=False)
+Proc1_1 = Process(env, "proc1", "(주)성광테크", proc_time, 5, 7, qlimit=10, limit_bytes=False)
+Proc1_2 = Process(env, "proc1", "건일산업(주)", proc_time, 5, 7, qlimit=10, limit_bytes=False)
+Proc1_3 = Process(env, "proc1", "부흥", proc_time, 5, 7, qlimit=10000, limit_bytes=False)
+Proc1_4 = Process(env, "proc1", "삼성중공업(주)거제", proc_time, 10, 7, qlimit=10, limit_bytes=False)
+Proc1_5 = Process(env, "proc1", "성일", proc_time, 5, 7, qlimit=10000, limit_bytes=False)
+Proc1_6 = Process(env, "proc1", "성일SIM함안공장", proc_time, 5, 7, qlimit=10, limit_bytes=False)
+Proc1_7 = Process(env, "proc1", "해승케이피피", proc_time, 5, 7, qlimit=10, limit_bytes=False)
 
-Proc2_1 = Process(env, "(주)성광테크", proc_time, 10, 1, qlimit=10000, limit_bytes=False)
-Proc2_2 = Process(env, "건일산업(주)", proc_time, 10, 1, qlimit=10000, limit_bytes=False)
-Proc2_3 = Process(env, "삼녹", proc_time, 10, 1, qlimit=10000, limit_bytes=False)
-Proc2_4 = Process(env, "성도", proc_time, 10, 1, qlimit=10000, limit_bytes=False)
-Proc2_5 = Process(env, "성일SIM함안공장", proc_time, 10, 1, qlimit=10000, limit_bytes=False)
-Proc2_6 = Process(env, "하이에", proc_time, 10, 1, qlimit=10000, limit_bytes=False)
-Proc2_7 = Process(env, "해승케이피", proc_time, 10, 1, qlimit=10000, limit_bytes=False)
+Proc2_1 = Process(env, "proc2", "(주)성광테크", proc_time, 5, 1, qlimit=10, limit_bytes=False)
+Proc2_2 = Process(env, "proc2", "건일산업(주)", proc_time, 5, 1, qlimit=10, limit_bytes=False)
+Proc2_3 = Process(env, "proc2", "삼녹", proc_time, 5, 1, qlimit=10, limit_bytes=False)
+Proc2_4 = Process(env, "proc2", "성도", proc_time, 5, 1, qlimit=10, limit_bytes=False)
+Proc2_5 = Process(env, "proc2", "성일SIM함안공장", proc_time, 5, 1, qlimit=10, limit_bytes=False)
+Proc2_6 = Process(env, "proc2", "하이에어", proc_time, 5, 1, qlimit=10, limit_bytes=False)
+Proc2_7 = Process(env, "proc2", "해승케이피피", proc_time, 5, 1, qlimit=10, limit_bytes=False)
+
+Monitor1_1 = Monitor(env, Proc1_1, samp_dist)
+Monitor1_2 = Monitor(env, Proc1_2, samp_dist)
+Monitor1_3 = Monitor(env, Proc1_3, samp_dist)
+Monitor1_4 = Monitor(env, Proc1_4, samp_dist)
+Monitor1_5 = Monitor(env, Proc1_5, samp_dist)
+Monitor1_6 = Monitor(env, Proc1_6, samp_dist)
+Monitor1_7 = Monitor(env, Proc1_7, samp_dist)
+
+Monitor2_1 = Monitor(env, Proc2_1, samp_dist)
+Monitor2_2 = Monitor(env, Proc2_2, samp_dist)
+Monitor2_3 = Monitor(env, Proc2_3, samp_dist)
+Monitor2_4 = Monitor(env, Proc2_4, samp_dist)
+Monitor2_5 = Monitor(env, Proc2_5, samp_dist)
+Monitor2_6 = Monitor(env, Proc2_6, samp_dist)
+Monitor2_7 = Monitor(env, Proc2_7, samp_dist)
 
 Source.outs[0] = Proc1_1
 Source.outs[1] = Proc1_2
@@ -149,3 +166,62 @@ Proc2_6.outs[0] = Sink
 Proc2_7.outs[0] = Sink
 
 env.run(until=RUN_TIME)
+
+print("Total Lead Time : ", Sink.last_arrival)
+
+# 공정별 가동
+print("utilization of Process1_1: {:2.2f}".format(Proc1_1.working_time/Sink.last_arrival))
+print("utilization of Process1_2: {:2.2f}".format(Proc1_2.working_time/Sink.last_arrival))
+print("utilization of Process1_3: {:2.2f}".format(Proc1_3.working_time/Sink.last_arrival))
+print("utilization of Process1_4: {:2.2f}".format(Proc1_4.working_time/Sink.last_arrival))
+print("utilization of Process1_5: {:2.2f}".format(Proc1_5.working_time/Sink.last_arrival))
+print("utilization of Process1_6: {:2.2f}".format(Proc1_6.working_time/Sink.last_arrival))
+print("utilization of Process1_7: {:2.2f}".format(Proc1_7.working_time/Sink.last_arrival))
+
+print("utilization of Process2_1: {:2.2f}".format(Proc2_1.working_time/Sink.last_arrival))
+print("utilization of Process2_2: {:2.2f}".format(Proc2_2.working_time/Sink.last_arrival))
+print("utilization of Process2_3: {:2.2f}".format(Proc2_3.working_time/Sink.last_arrival))
+print("utilization of Process2_4: {:2.2f}".format(Proc2_4.working_time/Sink.last_arrival))
+print("utilization of Process2_5: {:2.2f}".format(Proc2_5.working_time/Sink.last_arrival))
+print("utilization of Process2_6: {:2.2f}".format(Proc2_6.working_time/Sink.last_arrival))
+print("utilization of Process2_7: {:2.2f}".format(Proc2_7.working_time/Sink.last_arrival))
+
+#Sink.show_chart()
+
+# 공정별 대기시간의 합
+names = ["(주)성광테크", "건일산업(주)", "부흥", "삼성중공업(주)거제", "성일", "성일SIM함안공장", "해승케이피피", "삼녹", "성도", "하이에어"]
+waiting_time = {}
+for name in names:
+    t = 0
+    for i in range(len(Sink.waiting_list)):
+        if len(Sink.waiting_list[i]) == 2:
+            if name + ' ' + "waiting start" == list(Sink.waiting_list[i].keys())[0]:
+                t += Sink.waiting_list[i][name + " waiting finish"] - Sink.waiting_list[i][name + " waiting start"]
+                #print("2 ", Sink.waiting_list[i][name + " waiting finish"], " - ", Sink.waiting_list[i][name + " waiting start"])
+
+        elif len(Sink.waiting_list[i]) == 4:
+            #print(list(Sink.waiting_list[i].keys())[0])
+            #print(list(Sink.waiting_list[i].keys())[1])
+            #print(list(Sink.waiting_list[i].keys())[2])
+            #print(list(Sink.waiting_list[i].keys())[3])
+            if name + ' ' + "waiting start" == list(Sink.waiting_list[i].keys())[0]:
+                t += Sink.waiting_list[i][name + " waiting finish"] - Sink.waiting_list[i][name + " waiting start"]
+                #print("4 ", Sink.waiting_list[i][name + " waiting finish"], " - ", Sink.waiting_list[i][name + " waiting start"])
+            if name + ' ' + "waiting start" == list(Sink.waiting_list[i].keys())[2]:
+                t += Sink.waiting_list[i][name + " waiting finish"] - Sink.waiting_list[i][name + " waiting start"]
+                #print("4 ", Sink.waiting_list[i][name + " waiting finish"], " - ", Sink.waiting_list[i][name + " waiting start"])
+        else:
+            print(Sink.waiting_list[i])
+
+    waiting_time[name] = t
+
+print("total waiting time of (주)성광테크 : ",waiting_time["(주)성광테크"])
+print("total waiting time of 건일사업(주) : ",waiting_time["건일산업(주)"])
+print("total waiting time of 부흥 : ",waiting_time["부흥"])
+print("total waiting time of 삼성중공업(주)거제 : ",waiting_time["삼성중공업(주)거제"])
+print("total waiting time of 성일 : ",waiting_time["성일"])
+print("total waiting time of 성일SIM함안공장 : ",waiting_time["성일SIM함안공장"])
+print("total waiting time of 해승케이피피 : ",waiting_time["해승케이피피"])
+print("total waiting time of 삼녹 : ",waiting_time["삼녹"])
+print("total waiting time of 성도 : ",waiting_time["성도"])
+print("total waiting time of 하이에어 : ",waiting_time["하이에어"])
