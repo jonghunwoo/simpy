ae50ceb90d1d27567b9eb106d0b5f5725a0c0322
jonathan.woo <jonghun.digitalfactory@gmail.com>
2020-01-12 22:01:27 +0900

    revision

--- block_assembly.py
+++ block_assembly.py
@@ -8,6 +8,7 @@ import functools
 import simpy
 from SimComponents_for_block_assembly import Source, DataframeSource, Sink, Process, Monitor
 import matplotlib.pyplot as plt
+import numpy as np
 
 import time
 
@@ -75,13 +76,13 @@ Process6 = Process(env, 'Process6', proc_time, 1, qlimit=1, limit_bytes=False)
 Process7 = Process(env, 'Process7', proc_time, 1, qlimit=1, limit_bytes=False)
 
 # Using a PortMonitor to track each status over time
-Monitor1 = Monitor(env, Process1, samp_dist)
-Monitor2 = Monitor(env, Process2, samp_dist)
-Monitor3 = Monitor(env, Process3, samp_dist)
-Monitor4 = Monitor(env, Process4, samp_dist)
-Monitor5 = Monitor(env, Process5, samp_dist)
-Monitor6 = Monitor(env, Process6, samp_dist)
-Monitor7 = Monitor(env, Process7, samp_dist)
+Monitor1 = Monitor(env, 'Monitor1', Process1, samp_dist)
+Monitor2 = Monitor(env, 'Monitor2', Process2, samp_dist)
+Monitor3 = Monitor(env, 'Monitor3', Process3, samp_dist)
+Monitor4 = Monitor(env, 'Monitor4', Process4, samp_dist)
+Monitor5 = Monitor(env, 'Monitor5', Process5, samp_dist)
+Monitor6 = Monitor(env, 'Monitor6', Process6, samp_dist)
+Monitor7 = Monitor(env, 'Monitor7', Process7, samp_dist)
 
 # Connection
 Source.out = Process1
@@ -104,13 +105,17 @@ print("time :", time.time() - start)
 print('#'*80)
 print("Results of simulation")
 print('#'*80)
-print("Lead time of Last 10 Parts: " + ", ".join(["{:.3f}".format(x) for x in Sink.waits[-10:]]))
 
+#Sink.show_chart()
 
-# 총 리드타임 - 마지막 part가 Sink에 도달하는 시간
-print("Total Lead Time : ", Sink.last_arrival)
+print("Total Lead Time : ", Sink.last_arrival) # 총 리드타임 - 마지막 part가 Sink에 도달하는 시간
 
-# 공정별 대기시간의 합
+print("Sink: average lead time = {:.3f}".format(sum(Sink.waits)/len(Sink.waits))) # 모든 parts들의 리드타임의 평균
+#print("Lead time of Last 10 Parts: " + ", ".join(["{:.3f}".format(x) for x in Sink.waits[-10:]]))
+#print("Process1: Last 10 queue sizes: {}".format(Monitor1.sizes[-10:]))
+#print("Sink: Last 10 arrival times: " + ", ".join(["{:.3f}".format(x) for x in Sink.arrivals[-10:]])) # 모든 공정을 거친 assembly가 최종 노드에 도착하는 시간 간격 - TH 계산 가능
+
+## 공정별 대기시간의 합
 names = ["Process1", "Process2", "Process3", "Process4", "Process5", "Process6", "Process7"]
 waiting_time = {}
 for name in names:
@@ -119,36 +124,37 @@ for name in names:
         t += Sink.waiting_list[i][name + " waiting finish"] - Sink.waiting_list[i][name + " waiting start"]
     waiting_time[name] = t
 
-print("total waiting time of Process1 : ",waiting_time["Process1"])
-print("total waiting time of Process2 : ",waiting_time["Process2"])
-print("total waiting time of Process3 : ",waiting_time["Process3"])
-print("total waiting time of Process4 : ",waiting_time["Process4"])
-print("total waiting time of Process5 : ",waiting_time["Process5"])
+plt.bar(range(len(waiting_time)), list(waiting_time.values()), align='center')
+plt.title("Histogram for waiting time")
+plt.xlabel("Process")
+plt.ylabel("Waiting time")
+plt.show()
 
-#Sink.show_chart()
+## 공정별 가동
+processes = [Process1, Process2, Process3, Process4, Process5, Process6, Process7]
+utilization = {}
+for process in processes:
+    utilization[process.name] =  getattr(process, "working_time")/Sink.last_arrival
 
-print("utilization of Process1: {:2.2f}".format(Process1.working_time/Sink.last_arrival))
-print("utilization of Process2: {:2.2f}".format(Process2.working_time/Sink.last_arrival))
-print("utilization of Process3: {:2.2f}".format(Process3.working_time/Sink.last_arrival))
-print("utilization of Process4: {:2.2f}".format(Process4.working_time/Sink.last_arrival))
-print("utilization of Process5: {:2.2f}".format(Process5.working_time/Sink.last_arrival))
+plt.bar(range(len(utilization)), list(utilization.values()), align='center')
+plt.title("Histogram for utilization")
+plt.xlabel("Process")
+plt.ylabel("utilization")
+plt.show()
+
+## 공정별 평균 재공수률
+monitors = [Monitor1, Monitor2, Monitor3, Monitor4, Monitor5, Monitor6, Monitor7]
+WIP_avg = {}
+for monitor in monitors:
+    WIP_avg[monitor.name] = sum(getattr(monitor, "sizes"))/len(getattr(monitor, "sizes"))
+
+plt.bar(range(len(WIP_avg)), list(WIP_avg.values()), align='center')
+plt.title("Histogram for WIP")
+plt.xlabel("Process")
+plt.ylabel("Average WIP")
+plt.show()
 
 """
-print("Process1: Last 10 queue sizes: {}".format(Monitor1.sizes[-10:]))
-print("Process2: Last 10 queue sizes: {}".format(Monitor2.sizes[-10:]))
-print("Process3: Last 10 queue sizes: {}".format(Monitor3.sizes[-10:]))
-print("Process4: Last 10 queue sizes: {}".format(Monitor4.sizes[-10:]))
-print("Process5: Last 10 queue sizes: {}".format(Monitor5.sizes[-10:]))
-
-print("Sink: Last 10 arrival times: " + ", ".join(["{:.3f}".format(x) for x in Sink.arrivals[-10:]])) # 모든 공정을 거친 assembly가 최종 노드에 도착하는 시간 간격 - TH 계산 가능
-print("Sink: average lead time = {:.3f}".format(sum(Sink.waits)/len(Sink.waits))) # 모든 parts들의 리드타임의 평균
-
-print("average system occupancy of Process1: {:.3f}".format(float(sum(Monitor1.sizes))/len(Monitor1.sizes)))
-print("average system occupancy of Process2: {:.3f}".format(float(sum(Monitor2.sizes))/len(Monitor2.sizes)))
-print("average system occupancy of Process3: {:.3f}".format(float(sum(Monitor3.sizes))/len(Monitor3.sizes)))
-print("average system occupancy of Process4: {:.3f}".format(float(sum(Monitor4.sizes))/len(Monitor4.sizes)))
-print("average system occupancy of Process5: {:.3f}".format(float(sum(Monitor5.sizes))/len(Monitor5.sizes)))
-
 fig, axis = plt.subplots()
 axis.hist(Sink.waits, bins=100, density=True)
 axis.set_title("Histogram for waiting times")
